875ee150972d3143f5ade61f9f33d593516f3861
a791474 <a791474@gmail.com>
2024-02-20 12:24:11 +0800

    1. updateAdminState.php後台頁面Switch開關更新資料庫admin_state 2. backAdmin.php新增header允許標頭&傳接送方式

--- back/backAdminState.php
+++ back/backAdminState.php
@@ -1,6 +1,8 @@
 <?php
 header("Access-Control-Allow-Origin: *"); // 允許所有來源
 header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
 
 try {
     // 連線 MySQL
@@ -15,15 +17,15 @@ try {
     // 檢查是否有收到值
     if(isset($_POST['admin_state'])) {
         $admin_state = $_POST['admin_state']; // 假設接收到的是 0 或 1
-
         // SQL 更新
-        $sql = "UPDATE admin SET admin_state = :admin_state";  
+        $sql = "UPDATE admin SET admin_state = :admin_state WHERE admin_id = :admin_id";  
 
         // 準備 SQL 更新
         $updateAdminState = $pdo->prepare($sql);
-
+        $updateAdminState->bindValue(':admin_state',$_POST["admin_state"]);
+        $updateAdminState->bindValue(':admin_id',$_POST["admin_id"]);
         // 執行 SQL 更新
-        $updateAdminState->execute(array(':admin_state' => $admin_state));
+        $updateAdminState->execute();
 
         // 檢查是否有更新資料
         if ($updateAdminState->rowCount() > 0) {
