dcfa5456382c13887da3b4e74da3f67a3b5757d1
Kelly0518 <kelly05182@gmail.com>
2024-02-23 21:16:20 +0800

    add buydone

--- front/buyDone.php
+++ front/buyDone.php
@@ -11,19 +11,35 @@ try{
             require_once("https://tibamef2e.com/chd104/g6/api/connectChd104g6.php");
         }
 
+	// 检查必填字段是否為空
+    $requiredFields = array("ord_reciever", "ord_city", "ord_district", "ord_address", "ord_phone");
+    foreach ($requiredFields as $field) {
+        if (empty($_POST[$field])) {
+            $msg = "請填寫完整資訊才能完成訂購";
+            $result = array("msg" => $msg);
+            echo json_encode($result);
+            exit(); // 终止执行后续代码
+        }
+    }
 
 	// $memId = $_GET["member_id"];
-	$sql = "INSERT INTO ord_list (ord_reciever, ord_city, ord_district, ord_address, ord_phone, remark) VALUES 
-	(:ord_reciever, :ord_city, :ord_district, :ord_address, :ord_phone, :remark);";
+	$sql = "INSERT INTO ord_list (member_id, ord_reciever, ord_city, ord_district, ord_address, ord_phone, remark, ord_ship, ord_sum, ord_total, ord_del_state) VALUES 
+	(:member_id, :ord_reciever, :ord_city, :ord_district, :ord_address, :ord_phone, :remark, :ord_ship, :ord_sum, :ord_total, :ord_del_state);";
 	//編譯, 執行
 	$order = $pdo->prepare($sql);	
 
+	$order->bindValue(":member_id", $_POST["member_id"]);
 	$order->bindValue(":ord_reciever", $_POST["ord_reciever"]);
 	$order->bindValue(":ord_city", $_POST["ord_city"]);
 	$order->bindValue(":ord_district", $_POST["ord_district"]);
 	$order->bindValue(":ord_address", $_POST["ord_address"]);
 	$order->bindValue(":ord_phone", $_POST["ord_phone"]);
 	$order->bindValue(":remark", $_POST["remark"]);
+	$order->bindValue(":ord_ship", $_POST["ord_ship"]);
+	$order->bindValue(":ord_sum", $_POST["ord_sum"]);
+	$order->bindValue(":ord_total", $_POST["ord_total"]);
+	$order->bindValue(":ord_del_state", $_POST["ord_del_state"]);
+
 
 	$order->execute();
 
