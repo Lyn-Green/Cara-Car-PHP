d516daf2666c168c4812a845abbfb65c8ae4dc37
a791474 <a791474@gmail.com>
2024-02-24 14:33:32 +0800

    change database connection to AWS

--- connectChd104g6.php
+++ connectChd104g6.php
@@ -1,15 +1,28 @@
 <?php
 	//建立表單名稱,使用者名稱,密碼,port，建立$dsn去連線資料庫，建立options是表單設定,可用可不用，最後建立PDO物件 new PDO(驗證)
+
+
 	// $dbname = "tibamefe_chd104g6";
 	// $user = "tibamefe_since2021";
 	// $password = "vwRBSb.j&K#E";
+	// $dbname = "chd104g6";
+	// $user = "root";
+	// $password = "";
+	// $port ="3306";
+	// $dsn = "mysql:host=localhost;port={$port};dbname={$dbname};charset=utf8";;
+	// $options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE=>PDO::CASE_NATURAL);
+	// //建立pdo物件
+	// $pdo = new PDO($dsn, $user, $password, $options);
+
 	$dbname = "chd104g6";
-	$user = "root";
-	$password = "";
+	$user = "dev";
+	$password = "Chd104g6";
 	$port ="3306";
-	$dsn = "mysql:host=localhost;port={$port};dbname={$dbname};charset=utf8";;
+	$host = "chd104g6.ch44i2mqiwrj.ap-southeast-2.rds.amazonaws.com";
+	$dsn = "mysql:host={$host};port={$port};dbname={$dbname};charset=utf8";;
 	$options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE=>PDO::CASE_NATURAL);
-	//建立pdo物件
+
+	// //建立pdo物件
 	$pdo = new PDO($dsn, $user, $password, $options);
 
 
