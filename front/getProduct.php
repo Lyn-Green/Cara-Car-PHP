75d156508ca719eed00f7c6bbf140c362c0a8013
Kelly0518 <kelly05182@gmail.com>
2024-02-27 17:44:46 +0800

    fix cart php

--- front/getProduct.php
+++ front/getProduct.php
@@ -1,10 +1,13 @@
 <?php
+session_start();
 header("Access-Control-Allow-Origin:*");
 header("Content-Type:application/json;charset=utf-8");
 // $cart = file_get_contents("php://input");
 // $data = json_decode($cart, true);
 $cartArrayJSON = $_POST["cartArray"];
 $cartArray = json_decode($cartArrayJSON, true);
+
+// require_once("shared_ord_id.php");
 
 try{
 	    // 連線 MySQL
@@ -24,21 +27,30 @@ try{
 
 
 	// $memId = $_GET["member_id"];
-	$sql = "INSERT INTO ord_content (pro_id, pro_name, ord_qty, pro_price, promo_ratio, pro_sale, ord_sum) VALUES 
-	(:pro_id, :pro_name, :ord_qty, :pro_price, :promo_ratio, :pro_sale, :ord_sum);";
-	//編譯, 執行
-	$order = $pdo->prepare($sql);	
 
+	
 	foreach($cartArray as $item){
-		$order->bindValue(":pro_id", $item["pro_id"]);
-		$order->bindValue(":pro_name", $item["pro_name"]);
-		$order->bindValue(":ord_qty", $item["ord_qty"]);
-		$order->bindValue(":pro_price", $item["pro_price"]);
-		$order->bindValue(":promo_ratio", $item["promo_ratio"]);
-		$order->bindValue(":pro_sale", $item["pro_sale"]);
-		$order->bindValue(":ord_sum", $item["ord_sum"]);
 
+		$sql = "INSERT INTO ord_content (ord_id,pro_id, pro_name, ord_qty, pro_price, promo_ratio, pro_sale, ord_sum) VALUES 
+		(:ord_id,:pro_id, :pro_name, :ord_qty, :pro_price, :promo_ratio, :pro_sale, :ord_sum);";
+		//編譯, 執行
+		$order = $pdo->prepare($sql);	
+
+		// $ordId = $_SESSION['ordId'];
+		// $ordId = getOrdId();
+		
+		// $order->bindParam(":ord_id", $ordId);
+		$order->bindParam(":ord_id", $item["ord_id"]);
+		$order->bindParam(":pro_id", $item["pro_id"]);
+		$order->bindParam(":pro_name", $item["pro_name"]);
+		$order->bindParam(":ord_qty", $item["ord_qty"]);
+		$order->bindParam(":pro_price", $item["pro_price"]);
+		$order->bindParam(":promo_ratio", $item["promo_ratio"]);
+		$order->bindParam(":pro_sale", $item["pro_sale"]);
+		$order->bindParam(":ord_sum", $item["ord_sum"]);
 		$order->execute();
+
+
 	}
 	// $order->bindValue(":pro_id", $_POST["pro_id"]);
 	// $order->bindValue(":pro_name", $_POST["pro_name"]);
@@ -49,7 +61,7 @@ try{
     // $order->bindValue(":ord_sum", $_POST["ord_sum"]);
 
 	// $order->execute();
-
+	// $pdo->commit();
     $msg = "已抓取商品資訊";
 }
 catch (PDOException $e) {
