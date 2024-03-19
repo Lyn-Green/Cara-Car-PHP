ab85f651c33da0bcd2c81e58a4079d8dcc3967e3
a791474 <a791474@gmail.com>
2024-03-19 13:04:10 +0800

    停止AWS，修正api在本地端上使用的所有路徑和connection

--- connectChd104g6.php
+++ connectChd104g6.php
@@ -2,23 +2,23 @@
 	//建立表單名稱,使用者名稱,密碼,port，建立$dsn去連線資料庫，建立options是表單設定,可用可不用，最後建立PDO物件 new PDO(驗證)
 	
 	// 本地端localhost DataBase
-	// $dbname = "chd104g6";
-	// $user = "root";
-	// $password = "";
-	// $port ="3306";
+	$dbname = "tibamefe_chd104g6";
+	$user = "root";
+	$password = "";
+	$port ="3306";
 
 	// 緯育Tibame雲端DataBase
 	// $dbname = "tibamefe_chd104g6";
 	// $user = "tibamefe_since2021";
 	// $password = "vwRBSb.j&K#E";
 	// $port ="3306";
-	// $host = "localhost";
+	$host = "localhost";
 	// AWS的雲端DataBase
-	$dbname = "chd104g6";
-	$user = "dev";
-	$password = "Chd104g6";
-	$port ="3306";
-	$host = "chd104g6.ch44i2mqiwrj.ap-southeast-2.rds.amazonaws.com";
+	// $dbname = "chd104g6";
+	// $user = "dev";
+	// $password = "Chd104g6";
+	// $port ="3306";
+	// $host = "chd104g6.ch44i2mqiwrj.ap-southeast-2.rds.amazonaws.com";
 	$dsn = "mysql:host={$host};port={$port};dbname={$dbname};charset=utf8";;
 	$options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE=>PDO::CASE_NATURAL);
 
--- front/uploadMemberImg.php
+++ front/uploadMemberImg.php
@@ -12,8 +12,8 @@ try {
     $imgPath = $_POST['img_path'];
 
     // 指定前端資料夾路徑
-    // $frontendDirectory = 'C:/wamp64/www/g6/front/src/assets/imgs/memberImg/'; //本地端
-    $frontendDirectory = '../../imgs/memberImgs/'; //雲端
+    $frontendDirectory = 'C:/wamp64/www/g6/front/src/assets/imgs/memberImg/'; //本地端
+    // $frontendDirectory = '../../imgs/memberImgs/'; //雲端
 
     // 指定文件保存路徑
     $targetPath = $frontendDirectory . $imgPath;
